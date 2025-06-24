b55795d3266e5ff10310eb692e684612e4a01f7d
Nicholas Pease <me@nicholaspease.com>
2025-06-23 23:31:23 -0400

    feat: update about and home page attributions

--- src/components/dialogs/AboutDialog.vue
+++ src/components/dialogs/AboutDialog.vue
@@ -1,25 +1,29 @@
 <template>
   <q-dialog :model-value="show" @hide="$emit('hide')">
-    <q-card style="min-width: 400px; padding: 10px; border-radius: 10px">
-      <q-card-section>
-        <div class="text-h6">TART Tool</div>
-      </q-card-section>
+    <q-card style="min-width: 50%;" flat bordered>
+      <q-card-section horizontal>
+        <q-card-section style="min-width: 50%;">
+          <div class="text-h6">TART Tool</div>
+          <p><strong>Version: </strong>{{ version }}</p>
+          <p>
+            <strong>Source: </strong>
+            Adapted from <a href="https://github.com/tecoholic/ner-annotator" target="_blank">NER Annotator</a> into <a href="https://github.com/theSKAILab/TeAM-NER" target="_blank">TART Tool</a>
+          </p>
+          <p>
+            <strong>Author:</strong><br/>
+            Original Author @tecoholic (<a href="https://github.com/tecoholic" target="_blank">Github</a>, <a href="https://github.com/tecoholic" target="_blank">Twitter</a>)<br/>
+            Adapted by @theSKAILab Team (<a href="https://github.com/theSKAILab" target="_blank">Github</a>)<br/>
+            <span style="padding-left: 25px;">Nicholas Pease (<a href="https://github.com/npease18" target="_blank">Github</a>, <a href="https://nicholaspease.com" target="_blank">Website</a>)</span><br/>
+            <span style="padding-left: 25px;">Umayer Reza (<a href="https://github.com/umayerr" target="_blank">Github</a>)</span>
+          </p>
+        </q-card-section>
 
-      <q-card-section class="q-pt-none">
-        <p><strong>Version: </strong>{{ version }}</p>
-        <p>
-          <strong>Source: </strong>
-          Adapted from <a href="https://github.com/tecoholic/ner-annotator" target="_blank">NER Annotator</a> into <a href="https://github.com/theSKAILab/TeAM-NER" target="_blank">TART Tool</a>
-        </p>
-        <p>
-          <strong>Author:</strong><br/>
-          Original Author @tecoholic (<a href="https://github.com/tecoholic" target="_blank">Github</a>, <a href="https://github.com/tecoholic" target="_blank">Twitter</a>)<br/>
-          Adapted by @theSKAILab Team (<a href="https://github.com/theSKAILab" target="_blank">Github</a>)<br/>
-          <span style="padding-left: 25px;">Nicholas Pease (<a href="https://github.com/npease18" target="_blank">Github</a>, <a href="https://nicholaspease.com" target="_blank">Website</a>)</span><br/>
-          <span style="padding-left: 25px;">Umayer Reza (<a href="https://github.com/umayerr" target="_blank">Github</a>)</span>
-        </p>
+        <q-card-section>
+          This research was supported in part by the
+          U.S. Department of Agriculture through a National Institute of Food and Agriculture (NIFA) grant, Award #<a href="https://portal.nifa.usda.gov/web/crisprojectpages/1025810-cellulose-materials-informatics-building-a-knowledge-graph-for-cellulose-materials-discovery-and-research-cellograph.html" target="_blank">2021-67022-34366</a>
+          <br/><br/><img src="@/assets/forestserviceicon.png" style="width: 100%;"/>
+        </q-card-section>
       </q-card-section>
-
       <q-card-actions align="right">
         <q-btn flat label="OK" color="primary" v-close-popup />
       </q-card-actions>
--- src/components/pages/StartPage.vue
+++ src/components/pages/StartPage.vue
@@ -2,9 +2,15 @@
   <div class="q-mx-auto q-my-xl column items-center justify-center">
     <img src="@/assets/umaine-dark.png" alt="UMaine Logo" class="q-mb-sm" width="550px" v-show="$q.dark.isActive"/>
     <img src="@/assets/umaine.png" alt="UMaine Logo" class="q-mb-sm" width="550px" v-show="!$q.dark.isActive"/>
-    <h5 class="text-center text-h4 q-mb-none text-bold">Spatial Knowledge and Artificial Intelligence Lab</h5>
-    <h6 class="text-center text-h4 q-mb-none q-mt-lg">Text Annotation Review and Tagging (TART)</h6>
+    <h6 class="text-center text-h4 q-mb-none q-mt-lg text-bold">Text Annotation Review and Tagging (TART) Tool</h6>
     <h6 class="text-center text-h6 q-mb-none q-mt-xl text-italic">To Start, Open a File from the 'File' Menu</h6>
+    <div style="position: absolute; bottom: 20px;font-size: 16px;">
+      This research was supported in part by the U.S. Department of Agriculture through a National Institute of Food and Agriculture (NIFA) grant, Award #<a href="https://portal.nifa.usda.gov/web/crisprojectpages/1025810-cellulose-materials-informatics-building-a-knowledge-graph-for-cellulose-materials-discovery-and-research-cellograph.html" target="_blank">2021-67022-34366</a>
+      <div class="q-mt-md row items-center justify-center">
+        <img src="@/assets/skailabicon.png" style="height: 40px;padding-top: 2px;"/>
+        <img src="@/assets/forestserviceicon.png" style="margin-left: 13px; height: 50px;"/>
+      </div>
+    </div>
   </div>
 </template>
 
